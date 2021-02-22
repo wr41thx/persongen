d3d62d99c49b6c3d42fabaf687d2a21d2ee38a44
Theo <34557859+wr41thx@users.noreply.github.com>
2021-02-22 11:31:47 -0800

    Update person-generator.py

--- person-generator.py
+++ person-generator.py
@@ -11,11 +11,16 @@
 
 # If run form the command line with no argument, it launches a graphical user interface.
 
+# UPDATE 2/21/2021
+# The program now shares information with life-generator.py.  These files need to be located in a folder named "life_gen".
+# Still need to clean up and refactor code.
 
 from tkinter import *
 import csv
 import sys
 import random
+import subprocess
+import os.path
 
 """ BuildGUI creates the GUI and defines functions for buttons"""
 def BuildGUI():
@@ -27,15 +32,14 @@ def BuildGUI():
 
     """Submit Button"""
     def subClick():
-
+        
         """"Set up array for streets and error label"""
         streets = []
         errorLabel = Label(root, text="Error: Please enter a number to generate!")
         selectedStreet = StringVar()
 
-        """Error Message if no entry"""
-        if len(numGenEntry.get()) == 0:
-            errorLabel.pack()
+        """Do not submit if number field is empty"""
+        if len(numGenEntry.get()) == 0 or len(toyNumGenEntry.get()) == 0:
             return
 
         """Show a text response confirming entry after submission"""
@@ -78,35 +82,87 @@ def BuildGUI():
 
         csv_file.close()
 
-        """Label to explain drop down"""
+        """Create a toys.csv to send to life-generator"""
+        with open("toys.csv", "w+", newline='') as output_file:
+            print("Creating toys.csv for life-generator")
+            csv_writer = csv.writer(output_file, delimiter=",")
+            csv_writer.writerow(['input_item_type'] + ['input_item_category'] + ['input_number_to_generate'])
+            csv_writer.writerow(['toys'] + [selectedToy.get()] + [int(toyNumGenEntry.get())])
+        output_file.close()
+
+        """Run life-generator and generate output"""
+        print("searching for toys.csv...")
+        if os.path.exists('toys.csv'):
+            print("toy.csv found!")
+            print("Running life-generator.py with toys.csv")
+
+            """Run Life-Generator subprocess, this will output a csv file that i can take as further input"""
+            subprocess.call(["python", "./life_gen/life-generator.py", "toys.csv"], shell=True)
+            print("File output.csv written to ./life_gen!")
+
+            print("Appending life-gen output to person-gen output!")
+            toys = []
+            with open("./life_gen/output.csv", 'r') as csv_file:
+                csv_reader = list(csv.reader(csv_file, delimiter=','))
+                for line in csv_reader:
+                    toys.append(line)
+            csv_file.close()
+
+            """Isolate product names for GUI preview"""
+            toyPreview = []
+            isFirst = True
+            for line in toys:
+                if isFirst == True:
+                    isFirst = False;
+                else:
+                    toyPreview.append(line[3])
+
+        """Label for streets drop down"""
         cvsLabel = Label(root, text="Click the drop down below to preview addresses")
         cvsLabel.pack()
 
-        '''Create a drop down full of addresses as a preview and pack'''
+        """Create a drop down full of addresses as a preview and pack"""
         streetDrop = OptionMenu(root, selectedStreet, *streets)
         selectedStreet.set(streets[0])
         streetDrop.pack()
+
+        """Label for toy drop down"""
+        toyPreviewLabel = Label(root, text="\nClick the drop down to preview toys")
+        toyPreviewLabel.pack()
+
+        """Toy drop down"""
+        toyPrevDrop = OptionMenu(root, selectedToy, *toyPreview)
+        selectedToy.set(toyPreview[1])
+        toyPrevDrop.pack()
 
         """Clean up interface to only show relevant information"""
         submitButton.destroy()
         buttonLabel.destroy()
         numGenLabel.destroy()
+        toyNumGenLabel.destroy()
+        toyDrop.destroy()
         stateDrop.destroy()
         stateLabel.destroy()
+        toyLabel.destroy()
         myLabel2.destroy()
 
         """Outputs streets to output.csv"""
         def outputCSV():
-            print("Writing output.csv...")
+            print("Writing output.csv for person-generator")
             with open('output.csv', 'w+', newline='') as output_file:
                 csv_writer = csv.writer(output_file, delimiter=",")
                 csv_writer.writerow(['State'] + ['Number to Generate'] + ['Type'] + ['Value'])
 
                 for entry in streets:
                     csv_writer.writerow([selectedState.get()] + [int(numGenEntry.get())] + ["Street Address"] + [entry])
-
+                for entry in toys:
+                    csv_writer.writerow(entry)
             output_file.close()
+
+            """Remove entry fields"""
             numGenEntry.destroy()
+            toyNumGenEntry.destroy()
+
             outputLabel = Label(root, text="\nOutput complete, please check program directory!\n")
             outputLabel.pack()
             print("output.csv is now in same directory as program!")
@@ -119,7 +175,9 @@ def BuildGUI():
         restartButton = Button(root, text="Restart", command=restartGUI, fg="white", bg="red")
         restartButton.pack()
 
+    """*******************************************************************************************************"""
     """This is where the initial GUI creation starts"""
+    """*******************************************************************************************************"""
     root = Tk()
     root.title("Person Generator by Theodor Melsheimer")
     root.geometry("640x480")
@@ -148,9 +206,28 @@ def BuildGUI():
         "WA",
         "WY"
     ]
-
     selectedState = StringVar()
     selectedState.set(stateOptions[0])
+
+    """Toy Selections for drop down"""
+    toyOptions = [
+        "Arts & Crafts",
+        "Baby & Toddler Toys",
+        "Characters & Brands",
+        "Die-Cast & Toy Vehicles",
+        "Educational Toys",
+        "Electronic Toys",
+        "Figures & Playsets",
+        "Games",
+        "Jigsaws & Puzzles",
+        "Musical Toy Instruments",
+        "Novelty & Special Use",
+        "Pretend Play",
+        "Sports Toys & Outdoor"
+    ]
+    selectedToy = StringVar()
+    selectedToy.set(toyOptions[0])
+
 
     """Drop Down Box"""
     stateLabel = Label(root, text="Select State")
@@ -158,12 +235,23 @@ def BuildGUI():
     stateDrop = OptionMenu(root, selectedState, *stateOptions)
     stateDrop.pack()
 
-    """Number to generate input field"""
+    """Number to generate input field for streets"""
     numGenLabel = Label(root, text="\nNumber of Addresses to Generate")
     numGenLabel.pack()
-
     numGenEntry = Entry(root, width=10)
     numGenEntry.pack()
+
+    """Drop Down Box for Toy Selection"""
+    toyLabel = Label(root, text="\nSelect a Category")
+    toyLabel.pack()
+    toyDrop = OptionMenu(root, selectedToy, *toyOptions)
+    toyDrop.pack()
+
+    """Number to generate input field for toys"""
+    toyNumGenLabel = Label(root, text="\nNumber of Toys to Generate")
+    toyNumGenLabel.pack()
+    toyNumGenEntry = Entry(root, width=10)
+    toyNumGenEntry.pack()
 
     """Submit Button"""
     buttonLabel = Label(root, text="\n\n\n\n\n\nClick here after selection!")
@@ -255,6 +343,7 @@ if 1 < argCount < 3:
     while fillCount < int(genNum):
         randIndex = random.randrange(1, numLines - 50)
         randStreet = csv_reader[randIndex]
+
         """My attempt to clean some of this data up.  To be frank, these csv files are a huge mess :( """
         if randStreet[1] == "" or randStreet[1] is None or randStreet[1] == " " or randStreet[1] == "N/A":
             randIndex = random.randrange(1, numLines - 50)
@@ -265,7 +354,8 @@ if 1 < argCount < 3:
             fillCount += 1
 
         csv_file.close()
-    print("Writing output.csv...")
+
+    print("Writing output.csv for person-generator")
     with open('output.csv', 'w+', newline='') as output_file:
         csv_writer = csv.writer(output_file, delimiter=",")
         csv_writer.writerow(['State'] + ['Number to Generate'] + ['Type'] + ['Value'])
@@ -274,7 +364,34 @@ if 1 < argCount < 3:
             csv_writer.writerow([state] + [genNum] + ["Street Address"] + [entry])
 
     output_file.close()
-    print("output.csv is now in same directory as program!")
+    print("output.csv is now in same directory as person-generator.py!")
+
+    """Communication with life-generator.py for cmd interface starts here!"""
+
+    print("searching for toys.csv...")
+    if os.path.exists('toys.csv'):
+        print("toy.csv found!")
+        print("Running life-generator.py with toys.csv")
+
+        """Run Life-Generator subprocess, this will output a csv file that i can take as further input"""
+        subprocess.call(["python", "./life_gen/life-generator.py", "toys.csv"], shell=True)
+        print("File output.csv written to ./life_gen!")
+
+        print("Appending life-gen output to person-gen output!")
+        toys = []
+        with open("./life_gen/output.csv", 'r') as csv_file:
+            csv_reader = list(csv.reader(csv_file, delimiter=','))
+            for line in csv_reader:
+                toys.append(line)
+
+        with open("output.csv", 'a+', newline='') as output_file:
+            csv_writer = csv.writer(output_file, delimiter=',')
+            for line in toys:
+                csv_writer.writerow(line)
+
+    else:
+        print('toys.csv not found, closing person-generator.py!')
+
     exit(0)
 
 if argCount >= 3:
